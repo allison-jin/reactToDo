9bdc98719e970631ada1951cd15bac4abdccc2e7
Allison Jin <ajxiaoxi@gmail.com>
2018-11-09 05:58:52 -0800

    checkpoint 8 ReactToDoEvents: completed

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,15 +1,19 @@
 import React, {Component } from 'react';
 
 class ToDo extends Component {
+
+
   render(){
     return (
-      <li>{ this.props.description}</li>
+
       <li>
-        <input type = "checkbox" checked={ this.props.isCompleted} />
+        <input type = "checkbox" checked={ this.props.isCompleted} onChange={ this.props.toggleComplete} />
         <span> { this.props.description} </span>
+        <button onClick={this.props.deleteTodo}> Delete </button>
+
       </li>
     );
-  }
+  }ß
 }
 
 export default ToDo;
